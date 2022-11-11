0c0953e633585c0fa7b087b37a11b0f2e65eb361
AngryPanda160 <115058162+AngryPanda160@users.noreply.github.com>
2022-11-11 00:16:09 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,34 @@
-// TODO: create a component that displays a single bakery item
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+
+export default function BakeryItem(props) {
+  return (
+    <Card sx={{ width: 345, m: 1 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={props.image}
+        alt={props.name + " image"}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.name}
+        </Typography>
+        <Typography>{props.price}</Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.description}
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ justifyContent: "space-between" }}>
+        <Button variant="contained" onClick={props.addItem}>
+          Add to Cart
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
